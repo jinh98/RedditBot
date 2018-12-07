9b5f4f9eacf8e7722818c441be1c552560b722cf
Jin <jc7huang@edu.uwaterlo.ca>
2018-12-07 17:58:31 -0500

    added scripted comment bot

---
+++ config.py
@@ -0,0 +1,4 @@
+username = "unfulfilledVoid"
+password = "133233assa233133"
+client_id = "XJfwl7eq4Wbadg"
+client_secret = "aEjCULVFmxXgWDs79-zDy6z4-Is"
---
+++ reddit_bot.py
@@ -0,0 +1,57 @@
+import praw
+import config
+import time
+import os
+
+def bot_login():
+	print ("Connecting to Reddit")
+	#log bot into reddit
+	r = praw.Reddit(username = config.username,  password = config.password, client_id = config.client_id, client_secret = config.client_secret, user_agent = "Reddit spammer")
+	print ("Logged in!")
+	#if faled to login the program fails to run
+	return r
+
+
+def run_bot(r, comments_replied_to):
+	#Search for the last 1000 comments
+	print ("Looking for comments (max 1000)")
+
+	#sub reddit in bracket
+	for comment in r.subreddit('test').comments(limit=1000):
+
+		#serach for the comment that is not by the bot itself
+		if "shitpost" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
+			print ("String with \"shitpost\" found in comment " + comment.id)
+			comment.reply("dank")
+			print ("Replied to comment " + comment.id)
+
+			comments_replied_to.append(comment.id)
+
+			with open ("comments_replied_to.txt", "a") as f:
+				f.write(comment.id + "\n")
+
+	print ("Search Completed.")
+
+	print (comments_replied_to)
+
+	print ("Sleeping for 10 seconds...")
+	#Sleep for 10 seconds...		
+	time.sleep(10)
+
+def get_saved_comments():
+	if not os.path.isfile("comments_replied_to.txt"):
+		comments_replied_to = []
+	else:
+		with open("comments_replied_to.txt", "r") as f:
+			comments_replied_to = f.read()
+			comments_replied_to = comments_replied_to.split("\n")
+			comments_replied_to = filter(None, comments_replied_to)
+
+	return comments_replied_to
+
+r = bot_login()
+comments_replied_to = get_saved_comments()
+print (comments_replied_to)
+
+while True:
+	run_bot(r, comments_replied_to)
